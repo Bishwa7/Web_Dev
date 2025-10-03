dd1ec61a31971cd7ccdfe5cb12ce43a22dc51d95
Bishwa7 <bishwa244@gmail.com>
2025-10-04 03:49:43 +0530

    Week13 tailwindcss-project-one (grid)

--- Week13/tailwindcss-project-one/README.md
+++ Week13/tailwindcss-project-one/README.md
@@ -59,3 +59,34 @@ function App() {
   )
 }
 ```
+
+
+
+
+## Grid in tailwind css
+- Link to official documentation [Tailwind CSS Documentation](https://tailwindcss.com/docs/grid-template-columns)
+
+
+```javascript
+
+function App() {
+
+
+  return (
+    <>
+      <div className ='grid grid-cols-12'>
+        <div className='bg-red-300 col-span-2'>
+          CHILD 1
+        </div>
+        <div className='bg-green-300 col-span-8'>
+          CHILD 2
+        </div>
+        <div className='bg-blue-300 col-span-2'>
+          CHILD 3
+        </div>
+      </div>
+    </>
+  )
+}
+
+```
--- Week13/tailwindcss-project-one/src/App.jsx
+++ Week13/tailwindcss-project-one/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div className ='flex justify-around'>
-        <div className='bg-red-300'>CHILD 1</div>
-        <div className='bg-green-300'>CHILD 2</div>
-        <div className='bg-blue-300'>CHILD 3</div>
+      <div className ='grid grid-cols-12'>
+        <div className='bg-red-300 col-span-2'>
+          CHILD 1
+        </div>
+        <div className='bg-green-300 col-span-8'>
+          CHILD 2
+        </div>
+        <div className='bg-blue-300 col-span-2'>
+          CHILD 3
+        </div>
       </div>
     </>
   )
