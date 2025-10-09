95dcdb726d33c928262f11c902f524abc49e92a5
Bishwa7 <bishwa244@gmail.com>
2025-10-09 23:32:24 +0530

    Week14 react-typescript-interfaces completed

--- Week14/react-typescript-interfaces/src/App.tsx
+++ Week14/react-typescript-interfaces/src/App.tsx
@@ -1,11 +1,40 @@
 
 
 function App() {
+
+  const todo = {
+    title: "Go to gym",
+    description: "Today is push day",
+    done: false
+  }
   
   return (
     <>
-      
+    <Todo todo={todo}></Todo>
     </>
+  )
+}
+
+
+interface TodoType{
+  title: string,
+  description: string,
+  done?: boolean
+}
+
+
+interface TodoInput{
+  todo : TodoType
+}
+
+
+function Todo({todo}: TodoInput){
+
+  return (
+    <div>
+      <h1>{todo.title}</h1>
+      <p>{todo.description}</p>
+    </div>
   )
 }
 
