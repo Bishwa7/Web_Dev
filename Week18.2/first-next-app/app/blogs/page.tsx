68e1851030ae7423862922969a851a684524c934
Bishwa7 <bishwa244@gmail.com>
2026-05-04 18:02:15 +0530

    W18.2-S1 - Created first Nextjs App

---
+++ Week18.2/first-next-app/app/blogs/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function blogs() {
+
+    return (
+        <div className="flex justify-center items-center">
+            Topic: Learn how to create Nextjs App from the best dev.
+        </div>
+    )
+}
