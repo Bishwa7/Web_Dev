bfce66349367ef5aaa4ef862a6387761f36eddf0
Bishwa7 <bishwa244@gmail.com>
2026-05-11 12:13:28 +0530

    W18.2-S2 - SSR data fetching using axios

--- Week18.2/first-next-app/app/blogs/page.tsx
+++ Week18.2/first-next-app/app/blogs/page.tsx
@@ -1,10 +1,36 @@
+import axios from "axios"
 
 
-export default function blogs() {
+
+interface IPost {
+    id: number
+    title: string;
+    body: string;
+}
+
+
+function Post({title, body} : Omit<IPost, "id">) {
+    
+    return <div>
+        Title: {title} <br/>
+        Body: {body}
+    </div>
+}
+
+
+
+async function getPosts() {
+
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+    return response.data;
+}
+
+export default async function blogs() {
+    const posts = await getPosts()
 
     return (
-        <div className="flex justify-center items-center">
-            Topic: Learn how to create Nextjs App from the best dev.
+        <div className="pb-10">
+            {posts.map((post: IPost) => <Post key={post.id} title={post.title} body={post.body} /> )}
         </div>
     )
 }
